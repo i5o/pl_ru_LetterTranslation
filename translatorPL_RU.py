0314237c2f73064eb72aea37da181d3692545599
Piotr <piotrantosz@protonmail.ch>
2015-09-14 17:31:18 +0200

    First commit

---
+++ translatorPL_RU.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+print "--------------------------------------"
+print "Zamiennik polskich liter na rosyjskie"
+print "--------------------------------------"
+print "Podaj litere / litery (np. ju / je / mieki znak itp.)"
+letter = raw_input()
+letter = letter.lower()
+
+print "Rosyjski odpowiednik to: "
+
+if letter == "a":
+    print "Аа"
+elif letter == "b":
+    print "Бб"
+elif letter == "be":
+    print "Бб"
+elif letter == "w":
+    print "Вв"
+elif letter == "wy":
+    print "Вв"
+elif letter == "g":
+    print "Гг"
+elif letter == "gie":
+    print "Гг"
+elif letter == "d":
+    print "Дд"
+elif letter == "de":
+    print "Дд"
+elif letter == "je":
+    print "Ee"
+elif letter == "je":
+    print "Ee"
+elif letter == "jo":
+    print "Ёё"
+elif letter == "ż":
+    print "Жж"
+elif letter == "że":
+    print "Жж"
+elif letter == "żet":
+    print "Жж"
+elif letter == "z":
+    print "Зз"
+elif letter == "zy":
+    print "Зз"
+elif letter == "i":
+    print "Ии"
+elif letter == "j":
+    print "Йй"
+elif letter == "k":
+    print "Кк"
+elif letter == "ka":
+    print "Кк"
+elif letter == "l":
+    print "Лл"
+elif letter == "el":
+    print "Лл"
+elif letter == "ł":
+    print "Лл"
+elif letter == "eł":
+    print "Лл"
+elif letter == "m":
+    print "Мм"
+elif letter == "em":
+    print "Мм"
+elif letter == "n":
+    print "Нн"
+elif letter == "en":
+    print "Нн"
+elif letter == "o":
+    print "Oo"
+elif letter == "p":
+    print "Пп"
+elif letter == "pe":
+    print "Пп"
+elif letter == "r":
+    print "Рр"
+elif letter == "er":
+    print "Рр"
+elif letter == "ry":
+    print "Рр"
+elif letter == "s":
+    print "Cc"
+elif letter == "es":
+    print "Cc"
+elif letter == "es":
+    print "Cy"
+elif letter == "t":
+    print "Tt"
+elif letter == "te":
+    print "Tt"
+elif letter == "ty":
+    print "Tt"
+elif letter == "u":
+    print "Уу"
+elif letter == "f":
+    print "Фф"
+elif letter == "ef":
+    print "Фф"
+elif letter == "h":
+    print "Хх"
+elif letter == "ch":
+    print "Хх"
+elif letter == "ha":
+    print "Хх"
+elif letter == "c":
+    print "Цц"
+elif letter == "ce":
+    print "Цц"
+elif letter == "cz":
+    print "Чч"
+elif letter == "ć":
+    print "Чч"
+elif letter == "ćie":
+    print "Чч"
+elif letter == "cze":
+    print "Чч"
+elif letter == "sz":
+    print "Шш"
+elif letter == "sza":
+    print "Шш"
+elif letter == "szcz":
+    print "Щщ"
+elif letter == "szczy":
+    print "Щщ"
+elif letter == "szyczy":
+    print "Щщ"
+elif letter == "twardy znak":
+    print "Ъъ"
+elif letter == "twardy":
+    print "Ъъ"
+elif letter == "tz":
+    print "Ъъ"
+elif letter == "y":
+    print "Ыы"
+elif letter == "miekki znak":
+    print "Ьь"
+elif letter == "miękki znak":
+    print "Ьь"
+elif letter == "miękki":
+    print "Ьь"
+elif letter == "miekki":
+    print "Ьь"
+elif letter == "mz":
+    print "Ьь"
+elif letter == "e":
+    print "Ээ"
+elif letter == "ju":
+    print "Юю"
+elif letter == "ja":
+    print "Яя"
+else:
+    print "..chyba jednak coś źle wpisałeś. Spróbuj jeszcze raz."
+    
